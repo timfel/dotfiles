4c9f08b1344729a12e894363b181377d105acba2
Tim Felgentreff <timfelgentreff@gmail.com>
2009-02-05 00:05:06 +0100

    Allow to disable rdesktop or smb service tunneling by parameter

--- bin/hpi_shares.sh
+++ bin/hpi_shares.sh
@@ -7,19 +7,24 @@
 action="$1"
 smb="fs2"
 rdesktop="admin2"
-domainuser="rudolf.zirpmann"
-dhcpuser="rudi"
+nosmb=0
+nordesktop=0
+domainuser="tim.felgentreff"
+dhcpuser="timfel"
 
 ## No edit below this line
+smbparam=""
+rdesktopparam=""
 
 function usage {
 	echo "Usage:"
-	echo "hpi_shares.sh (start|stop) [--samba=SAMBA-SERVER] [--rdesktop=RDESKTOP-SERVER]"
+	echo "hpi_shares.sh (start|stop) [--samba=SAMBA-SERVER] [--rdesktop=RDESKTOP-SERVER] [--no-smb] [--no-rdesktop]"
 	echo
 	echo "start|stop has to come first, others can be arbitary, but only one"
 	echo "It will prompt for hpi domain password twice, and for dhcp-server password"
 	echo "You have to detach from each screen manually (Usually \"C-a d\")"
 	echo
+
 	exit 0
 }
 
@@ -27,30 +32,52 @@ function parse {
    var=$1
    if test $(echo "$var" | grep '\-\-samba=' ); then
       smb=$(echo "$var" | sed 's/--samba=//g')
-   else 
-      if test $(echo "$var" | grep '\-\-rdesktop=' ); then
-	 rdesktop=$(echo "$var" | sed 's/--rdesktop=//g')
-      else
-	 usage
-      fi
+   else if test $(echo "$var" | grep '\-\-rdesktop=' ); then
+      rdesktop=$(echo "$var" | sed 's/--rdesktop=//g')
+   else if test $(echo "$var" | grep '\-\-no\-smb' ); then
+      nosmb=1
+   else if test $(echo "$var" | grep '\-\-no\-rdesktop' ); then
+      nordesktop=1
+   fi
+   fi
+   fi
    fi
 }
+
+function usesmb {
+   if test "$(ps -C smbd | grep smbd)"; then
+      sudo /etc/init.d/samba stop
+   fi
+
+   smbparam="-L 139:$smb:139"
+}
+
+function userdesktop {
+   if [ $nordesktop -eq 0 ]; then
+      rdesktopparam="-L 3389:$rdesktop:3389"
+   fi
+}
+
 
 function start {
    smb=$1
    rdesktop=$2
 
-   echo "Starting as tunnel to smb://$smb and rdesktop://$rdesktop"
-
-   if test "$(ps -C smbd | grep smbd)"; then
-      sudo /etc/init.d/samba stop
+   if [ $nosmb -eq 1 && $nordesktop -eq 1 ]; then
+      echo "You chose to not use any service. No point in a tunnel. Breaking..."
    fi
+
+   echo "Starting as tunnel to smb://$smb and rdesktop://$rdesktop"
 
    screen -S hpi-tunnel ssh -L 12345:placebo:22 "$domainuser"@ssh-stud.hpi.uni-potsdam.de
    screen -S hpi-dhcp ssh -L 12346:dhcpserver:22 -p 12345 "$domainuser"@127.0.0.1
 
-   # have to run this as root, 139 is a protected port
-   sudo screen -S hpi-fs ssh -L 139:"$smb":139 -L 3389:"$rdesktop":3389 -p 12346 "$dhcpuser"@127.0.0.1
+   if [ $nosmb -eq 0 ]; then
+      # have to run this as root, 139 is a protected port
+      sudo screen -S hpi-fs ssh "$smbparam" "$rdesktopparam" -p 12346 "$dhcpuser"@127.0.0.1
+   else
+      screen -S hpi-fs ssh "$rdesktopparam" -p 12346 "$dhcpuser"@127.0.0.1
+   fi
 }
 
 function stop {
@@ -61,16 +88,13 @@ function stop {
    screen -S hpi-tunnel -X kill
 }
 
-if [ $# -eq 2 ]; then
-   parse $2
-else if [ $# -eq 3 ]; then
-      parse $2
-      parse $3
-   else if [ $# -ne 1 ]; then
-	 usage
-      fi
-   fi
+if [ $# -le 1 ]; then
+   usage
 fi
+
+for i in $@; do
+   parse $i;
+done
 
 if [ "$action" = "start" ]; then
    if test "$(screen -ls | grep 'hpi')"; then
