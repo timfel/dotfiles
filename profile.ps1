9a1d1be63bc6f0566bbf644be038026605e30355
Tim Felgentreff <timfelgentreff@gmail.com>
2020-09-28 19:41:50 +0200

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,7 +1,37 @@
 New-Alias unzip Expand-Archive -option AllScope -force
-New-Alias zip Compress-Archive -option AllScope -force
-New-Alias man Get-Help -option AllScope -force
-New-Alias wget Invoke-WebRequest -option AllScope -force
+New-Alias zip Compress-Archive -option AllScope  -force
+New-Alias man Get-Help -option AllScope  -force
+New-Alias wget Invoke-WebRequest -option AllScope  -force
+
+function vscode {
+    if ($args.Count -eq 0) {
+        Start-Process -WindowStyle Hidden code
+    } else {
+        Start-Process -WindowStyle Hidden code -ArgumentList "$args"
+    }
+}
+
+function first_time_system_setup {
+    $packages = @(
+        'keepass'
+        'steam'
+        'gog.galaxy'
+        'discord'
+        # 'uplay'
+        'Microsoft.VisualStudio.BuildTools'
+        'vlc'
+        'vscode'
+        'git'
+        'github.cli'
+        'slack'
+        'vncviewer'
+    )
+    foreach ($item in $packages) {
+        winget install "$item"
+    }
+
+    wsl --install
+}
 
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
