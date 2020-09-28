de0f09bab1859a6cc613818560cc5797f27df9d3
Tim Felgentreff <timfelgentreff@gmail.com>
2020-09-28 19:24:23 +0200

    Create profile.ps1

---
+++ profile.ps1
@@ -0,0 +1,14 @@
+New-Alias unzip Expand-Archive -force
+New-Alias zip Compress-Archive -force
+New-Alias man Get-Help -force
+New-Alias wget Invoke-WebRequest -force
+
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+    param($wordToComplete, $commandAst, $cursorPosition)
+        [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+        $Local:word = $wordToComplete.Replace('"', '""')
+        $Local:ast = $commandAst.ToString().Replace('"', '""')
+        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
