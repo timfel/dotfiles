a5362070acdda0720dfa4032a57f9dfac0754f7d
Tim Felgentreff <timfelgentreff@gmail.com>
2021-05-17 13:34:18 +0200

    minor fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -ex
+sudo apt install build-essential libssl-dev zlib1g-dev
 dir=`readlink -f $(dirname $0)`
 ln -sf "$dir"/.bash_profile $HOME/.bash_profile
 ln -sf "$dir"/.bashrc $HOME/.bashrc
