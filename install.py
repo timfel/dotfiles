546519026d011632cae18db610cbc10d4bb6e3ae
Tim Felgentreff <tim.felgentreff@oracle.com>
2026-05-22 16:02:59 +0200

    Install symlinks better

--- install.py
+++ install.py
@@ -1,5 +1,5 @@
 from glob import glob
-from os import makedirs, chdir, system, walk, getcwd, unlink, symlink
+from os import makedirs, chdir, system, walk, getcwd, unlink, symlink, readlink
 from os.path import join, basename, isdir, expanduser, relpath, dirname, exists, islink
 
 
@@ -9,9 +9,20 @@ system("git submodule update --init --recursive")
 dotfiles = glob(".*") + ["bin", "texmf"]
 
 
+def install_link(src, dst):
+    if islink(src):
+        src = readlink(src)
+    else:
+        src = relpath(src, dirname(dst))
+    if exists(dst) or islink(dst):
+        unlink(dst)
+    print(dst, "->", src)
+    symlink(src, dst)
+
+
 for dotfile in dotfiles:
     dst = join(expanduser("~"), dotfile)
-    if isdir(dotfile) and (
+    if not islink(dotfile) and isdir(dotfile) and (
             basename(dotfile).startswith(".") or
             isdir(dst)
     ):
@@ -20,18 +31,16 @@ for dotfile in dotfiles:
             if dirpath.startswith(".git"):
                 continue
             makedirs(join(expanduser("~"), dirpath), exist_ok=True)
+            for dirname_ in list(dirnames):
+                src = join(dirpath, dirname_)
+                if islink(src):
+                    dst = join(expanduser("~"), dirpath, dirname_)
+                    install_link(src, dst)
+                    dirnames.remove(dirname_)
             for filename in filenames:
                 dst = join(expanduser("~"), dirpath, filename)
                 src = join(dirpath, filename)
-                src = relpath(src, dirname(dst))
-                if exists(dst) or islink(dst):
-                    unlink(dst)
-                print(dst, "->", src)
-                symlink(src, dst)
+                install_link(src, dst)
     else:
         dst = join(expanduser("~"), dotfile)
-        src = relpath(dotfile, dirname(dst))
-        if exists(dst) or islink(dst):
-            unlink(dst)
-        print(dst, "->", src)
-        symlink(src, dst)
+        install_link(dotfile, dst)
